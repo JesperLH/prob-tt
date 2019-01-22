70660c6889b4594e1c91d772a182e681e268cc96
JesperLH <JesperLH@users.noreply.github.com>
2019-01-21 22:07:03 -0500

    updated display

--- tt_prob_tensor.m
+++ tt_prob_tensor.m
@@ -1,25 +1,26 @@
-function [G, ES, EV, Etau] = tt_prob_tensor(X,G, D, varargin)
+function [G, ES, EV, Etau, elbo] = tt_prob_tensor(X,G, D, varargin)
 %%TT_PROB_TENSOR Finds a probabilistic Tensor Train for Data X
 %% Read input and Setup Parameters
 paramNames = {'conv_crit', 'maxiter', ...
     'model_tau', 'fixed_tau', 'tau_a0', 'tau_b0',...
     'model_S', 'fixed_S',...
     'model_V', 'fixed_V',...
-    'model_G', 'fixed_G'};
-defaults = {1e-9, 100, ...
+    'model_G', 'fixed_G','verbose'};
+defaults = {1e-8, 100, ...
     true, 0, 1e-3, 1e-3,...
     true, 0,...
     true, 0,...
     true, 0,...
-	};
+	'yes'};
 % Initialize variables to default value or value given in varargin
 [conv_crit, max_iter, ...
     model_tau, fixed_tau, tau_alpha0, tau_beta0,...
-    model_S, fixed_S, model_V, fixed_V, model_G, fixed_G]...
+    model_S, fixed_S, model_V, fixed_V, model_G, fixed_G, verbose]...
     = internal.stats.parseArgs(paramNames, defaults, varargin{:});
 
 %% Initialize
-prior_s = ones(D(end-1),1)*1e+1;
+SST = sum(sum(X(:).^2));
+prior_s = ones(D(end-1),1)*sqrt(SST/numel(X));
 s_prior_log_value = ones(D(end-1),1)*0;
 s_lb = zeros(D(end-1),1);
 s_ub = ones(D(end-1),1)*inf;
@@ -56,14 +57,36 @@ else
 end
 
 Etau=tau_alpha0/tau_beta0;
-%% Run algo
 
-SST = sum(sum(X(:).^2));
+%%
+%% Setup how information should be displayed.
+if ~strcmpi(verbose,'no')
+    disp([' '])
+    disp(['Probabilistic Tensor Train'])        %TODO: Better information about what is running..
+    disp(['A D=' strcat('(',strjoin(strsplit(num2str([D(:); 1]')),', '), ')') ' component model will be fitted']);
+    disp([' ']);
+    dheader = sprintf(' %16s | %16s | %16s | %16s | %16s | %16s | %16s |','Iteration', ...
+        'Cost', 'rel. \Delta cost', 'Noise (s.d.)', 'Var. Expl.', 'Time (sec.)', 'Time CPU (sec.)');
+    dline = repmat('------------------+',1,7);
+    fprintf('%s\n%s\n%s\n',dline, dheader, dline);
+end
+
+%
+iter = 0;
+elbo = zeros(max_iter,1,'like', SST);
+rmse = zeros(max_iter,1,'like', SST);
+total_realtime = tic;
+total_cputime = cputime;
+
+%% Run algo
 iter = 0;
 dELBO = inf;
 
+
 while iter < max_iter && dELBO > conv_crit
     iter = iter+1;
+    time_tic_toc = tic;
+    time_cpu = cputime;
     %% Update each factor...
     if iter == 1 || model_G
         for i = 1:ndims(X)-1
@@ -74,17 +97,28 @@ while iter < max_iter && dELBO > conv_crit
             x_con = contract_fixed_modes(X,G,i);
             
             assert(sum(abs(x_con(:)))>0, 'No residual..')
-            
             % Update von-Mises-Fisher Distribution
             F=reshape(x_con, prod(sG(1:end-1)), sG(end))*Etau;
+            
+            % F is obs x comp, where it is desired that comp x comp is
+            % orthogonal
             [UU,SS,VV]=svd(F, 'econ');
-            [f,V,lF]=hyperg(N(i),diag(SS),3);
-            assert(~isinf(lF))
+            [f,V,lF]=hyperg(size(F,1),diag(SS),3);
+%             f = f/max(f);
+            
+            assert(max(f)>eps, 'Subspace is pruned...')
+            Gi_old = permute(G{i},[3,1,2]); Gi_old = Gi_old(:,:)'; 
+%             disp('Gi old')
+%             disp(Gi_old'*Gi_old)
             
             % Get expected value of G{i} and the entropy of vMF
             G{i}=reshape(UU*diag(f)*VV', sG);
-            H_G(i)= lF-sum(F(:).*G{i}(:));  %#ok<AGROW>
-
+            H_G(i)= lF-sum(sum(F(:).*G{i}(:)));  %#ok<AGROW>
+            %H_G(i) = sum(sum(F.*(UU*diag(f)*VV')'));
+            Gi_new = permute(G{i},[3,1,2]); Gi_new = Gi_new(:,:)'; 
+%             disp('Gi new')
+%             disp(Gi_new'*Gi_new)
+            
             assert(sum(abs(G{i}(:)))>0, 'An entire cart was turned off')
         end
     end
@@ -95,14 +129,14 @@ while iter < max_iter && dELBO > conv_crit
     % Update V
     if iter == 1 || model_V
         % Update von-Mises-Fisher Distribution
-        F=ES*x_con*Etau;
+        F=(ES*x_con)'*Etau;
         [UU,SS,VV]=svd(F, 'econ');
         [f,V,lF]=hyperg(N(end),diag(SS),3);
         assert(~isinf(lF))
         
         % Get expected value of G{i} and the entropy of vMF
-        EV=(UU*diag(f)*VV')';
-        H_V= lF-sum(sum(F.*EV'));  %#ok<AGROW>
+        EV=(UU*diag(f)*VV');
+        H_V= lF-sum(sum(F.*EV));  %#ok<AGROW>
         P_V = 0;
     end
     
@@ -115,7 +149,7 @@ while iter < max_iter && dELBO > conv_crit
         [logZhatOut, ~ , s_mu, s_var ] = ...
             truncNormMoments_matrix_fun(s_lb, s_ub, ss_mu , ss_sig2);
         
-        N_s = N(end);%1; %or D_M-1? or numel(X_rele)
+        N_s = N(end);
         
         [P_S, H_S] = getPriorAndEntropy_tnorm(s_mu, s_var, logZhatOut, N_s, ...
             s_prior_log_value, prior_s);
@@ -155,35 +189,48 @@ while iter < max_iter && dELBO > conv_crit
         +P_V+H_V...
         +P_S+sum(H_S);
   
-    cost(iter) = 0.5*SSE;
+    rmse(iter) = 0.5*SSE/numel(X);
+    %% Display
+    if mod(iter,50)==0 && ~strcmpi(verbose,'no')
+        fprintf('%s\n%s\n%s\n',dline, dheader, dline);
+    end
     
+    time_tic_toc = toc(time_tic_toc);
+    time_cpu = cputime-time_cpu;
     if iter > 1
-        dCost = (cost(iter)-cost(iter-1))/abs(cost(iter-1)); % Should decrease
+        dCost = (rmse(iter)-rmse(iter-1))/abs(rmse(iter-1)); % Should decrease
         dELBO = (elbo(iter)-elbo(iter-1))/abs(elbo(iter-1)); % Shoud increase
-        
-        fprintf('Iter: %4i, rCost: %6.4f,\t dCost: %6.4e,\t  ELBO: %6.4e,\t dELBO: %6.4e,\t tau: %6.4e\n',...
-            iter, cost(iter)/(0.5*SST), dCost,...
-            elbo(iter), dELBO, Etau);
+%         fprintf('Iter: %4i, rCost: %6.4f,\t dCost: %6.4e,\t  ELBO: %6.4e,\t dELBO: %6.4e,\t tau: %6.4e\n',...
+%             iter, rmse(iter)/(0.5*SST), dCost,...
+%             elbo(iter), dELBO, Etau);
+        if ~strcmpi(verbose,'no')
+            fprintf(' %16i | %16.4e | %16.4e | %16.4e | %16.4f | %16.4f | %16.4f |\n',...
+                iter, elbo(iter), dELBO, sqrt(1./Etau), 1-SSE/SST, time_tic_toc, time_cpu);
+        end
         
         if abs(dELBO) > conv_crit
-            assert(dELBO>=0, 'ELBO DECREASED!')
+           assert(dELBO>=0, 'ELBO DECREASED!')
         elseif dELBO < 0 
             warning('ELBO decreased, but was below convergence threshold')
         end
             
         
-    else
-        fprintf('Cost: %6.4e,\t  ELBO: %6.4e\n',...
-            cost(iter), elbo(iter));
+    elseif ~strcmpi(verbose,'no')
+        fprintf(' %16i | %16.4e | %16.4e | %16.4e | %16.4f | %16.4f | %16.4f |\n',...
+            iter, elbo(iter), nan, sqrt(1./Etau), 1-SSE/SST, time_tic_toc, time_cpu);
     end
    
     
     
 end
 
-if abs((2*cost(iter)/SST)-1) < eps
+if abs((2*rmse(iter)/SST)-1) < eps
     warning('Algorithm converged, but the Tensor Train does not describe the data!!!!!')
 end
+
+elbo = elbo(1:iter);
+rmse = rmse(1:iter);
+
 end
 
 function X_contr = contract_fixed_modes(X_contr,G, idx)
@@ -192,29 +239,29 @@ n_modes = ndims(X_contr);
 
 for i = 1:n_modes
     if i == 1 && i < idx
-        X_contr2 = tprod(X_contr, [-1, i+1:ndims(X_contr)], squeeze(G{i}), [-1, 1]);
+        X_contr = tprod(X_contr, [-1, i+1:ndims(X_contr)], squeeze(G{i}), [-1, 1]);
     elseif i < idx
-        X_contr2 = tprod(X_contr, [-1,-2, 2:ndims(X_contr)-1], G{i}, [-1, -2, 1]);
+        X_contr = tprod(X_contr, [-1,-2, 2:ndims(X_contr)-1], G{i}, [-1, -2, 1]);
     elseif i > idx
         if i == idx+1
             if idx == 1
-                X_contr2 = tprod(X_contr, [1,-1, 4:ndims(X_contr)+1], G{i}, [2, -1, 3]);
+                X_contr = tprod(X_contr, [1,-1, 4:ndims(X_contr)+1], G{i}, [2, -1, 3]);
             else
-                X_contr2 = tprod(X_contr, [1,2,-1, 5:ndims(X_contr)+1], G{i}, [3, -1, 4]);
+                X_contr = tprod(X_contr, [1,2,-1, 5:ndims(X_contr)+1], G{i}, [3, -1, 4]);
             end
         else
             if idx == 1
-                X_contr2 = tprod(X_contr, [1,2,-1,-2, 4:ndims(X_contr)-1], G{i}, [-1, -2, 3]);
+                X_contr = tprod(X_contr, [1,2,-1,-2, 4:ndims(X_contr)-1], G{i}, [-1, -2, 3]);
             else
-                X_contr2 = tprod(X_contr, [1,2,3,-1,-2, 5:ndims(X_contr)-1], G{i}, [-1, -2, 4]);
+                X_contr = tprod(X_contr, [1,2,3,-1,-2, 5:ndims(X_contr)-1], G{i}, [-1, -2, 4]);
             end
         end
     else
         
     end
-    if idx ~=1 || i > 1
-        X_contr=X_contr2;
-    end
+%     if idx ~=1 || i > 1
+%         X_contr=X_contr;
+%     end
 end
 
 end
