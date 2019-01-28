6d5c928a7a2bc1eb242f2e994c0da27220f71c30
JesperLH <JesperLH@users.noreply.github.com>
2019-01-28 14:28:04 -0500

    Updated run script

--- run_experiments_eusipco.m
+++ run_experiments_eusipco.m
@@ -4,13 +4,16 @@ addpath(genpath('../ncptensor'))
 addpath(genpath('../tools'))
 
 elapsed_time = zeros(6,1)*nan;
-for i = 5:6
+for j = 2:2
     t0 = tic;
-    switch i
+    switch j
         case 1
             experiments_aminoacid
         case 2
-            experiments_aminoacid_modelorder
+            perm_idx = perms(1:3);
+            for p = 1:size(perm_idx,1)
+                experiments_aminoacid_modelorder(perm_idx(p,:))
+            end
         case 3
             experiments_eusipco_tensortrain_nonoise
         case 4
@@ -20,5 +23,5 @@ for i = 5:6
         case 6
             experiments_eusipco_unknownD
     end
-    elapsed_time(i) = toc(t0);
+    elapsed_time(j) = toc(t0);
 end
