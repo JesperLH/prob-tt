7bc94235cb412a8264e2dfa046ed03d64ba9d471
JesperLH <JesperLH@users.noreply.github.com>
2019-01-28 22:40:10 -0500

    Plot confidence intervals

---
+++ plotCI.m
@@ -0,0 +1,16 @@
+function plotCI(x,mu,ci,plot_color, fill_color)
+
+if nargin < 4
+    plot_color = 'k';
+    fill_color = 'r';
+end
+
+x_plot =[x, fliplr(x)];
+y_plot=[ci(:,1)', flipud(ci(:,2))'];
+
+hold on
+plot(x, mu, 'color', plot_color, 'linewidth', 1)
+fill(x_plot, y_plot, 1,'facecolor', fill_color, 'edgecolor', 'none', 'facealpha', 0.4);
+hold off
+
+end
